9d0e1ace0210b5ac0aef402f93471c54fbfc5306
Andrew Taylor <aftaylor2@gmail.com>
2021-01-16 20:03:57 -0500

    Added ALE config

--- editors/neovim.sh
+++ editors/neovim.sh
@@ -212,6 +212,11 @@ let g:ale_fixers = {
 
 " Set this variable to 1 to fix files when you save them.
 let g:ale_fix_on_save = 1
+" let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
+let g:ale_sign_error = '✘'
+let g:ale_sign_warning = '⚠'
+let g:ale_lint_on_text_changed = 'never'
+
 let g:airline#extensions#ale#enabled = 1
 
 EOT
