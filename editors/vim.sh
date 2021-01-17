e8f651ebf80a4bb82873c5f1eb66cd627b9c2367
Andrew Taylor <aftaylor2@gmail.com>
2021-01-16 20:03:25 -0500

    Added ALE config

--- editors/vim.sh
+++ editors/vim.sh
@@ -99,6 +99,12 @@ let g:ale_fixers = {
   \ }
 
 let g:ale_fix_on_save = 1
+let g:ale_fix_on_save = 1
+" let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
+let g:ale_sign_error = '✘'
+let g:ale_sign_warning = '⚠'
+let g:ale_lint_on_text_changed = 'never'
+
 let g:DevIconsEnableFolderExtensionPatternMatching = 1
 let g:prettier#config#print_width = '80'
 let g:prettier#config#single_quote = 'true'
